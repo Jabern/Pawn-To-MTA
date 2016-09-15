b06321434c4adb1245ef1f591f3797ef91d11a3f
Jabern <jaberlama123@gmail.com>
2016-09-15 21:44:21 +0100

    Basic stuff

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "main.h"
+
+ILuaModuleManager10 *pModuleManager = NULL;
+
+MTAEXPORT bool InitModule(ILuaModuleManager10 *pManager, char *szModuleName, char *szAuthor, float *fVersion)
+{
+	pModuleManager = pManager;
+
+	// Set the module info
+	strncpy(szModuleName, MODULE_NAME, MAX_INFO_LENGTH);
+	strncpy(szAuthor, MODULE_AUTHOR, MAX_INFO_LENGTH);
+	(*fVersion) = MODULE_VERSION;
+
+	return true;
+}
+
+
+MTAEXPORT void RegisterFunctions(lua_State * luaVM)
+{
+	if (pModuleManager && luaVM)
+	{
+		pModuleManager->RegisterFunction(luaVM, "system", CFunctions::System);
+	}
+}
+
+
+MTAEXPORT bool DoPulse(void)
+{
+	return true;
+}
+
+MTAEXPORT bool ShutdownModule(void)
+{
+	return true;
+}
+
+
+MTAEXPORT bool ResourceStopping(lua_State * luaVM)
+{
+	return true;
+}
+
+MTAEXPORT bool ResourceStopped(lua_State * luaVM)
+{
+	return true;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#define MODULE_NAME         "Pawn Module"
+#define MODULE_AUTHOR       "Jaber"
+#define MODULE_VERSION      1.0
+
+#include <string.h>
+#include "CFunctions.h"
+#include "Common.h"
+#include "include\ILuaModuleManager.h"
